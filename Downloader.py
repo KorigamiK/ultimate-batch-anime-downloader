6a648edfbfb8b10047df857cff8f4961ffd1f77f
KorigamiK <72932688+KorigamiK@users.noreply.github.com>
2020-10-20 09:42:09 +0530

    Update Downloader.py

--- Downloader.py
+++ Downloader.py
@@ -1,44 +1,58 @@
+from typing import List
 from bs4 import BeautifulSoup as bs
 import requests
 import csv
 import os
 import wget
 
-src_url = input('enter url example (https://yugenani.me/anime/akudama-drive/watch/): ')
+while True:
+    src_url = input('enter url example (https://yugenani.me/anime/akudama-drive/watch/): ')
+    try:
+        src_url.split('/')[-2] == 'watch'
+        break
+    except:
+        print('Give a correct url (check for /watch/ at the end)')
+
+print('\nGetting Episodes\n')
+
 src = requests.get(src_url).text
 soup = bs(src, 'lxml')
 
 try:
     last_page = int(soup.find_all('a', class_="btn btn-flat btn-small")[-2].text)
-except Exception:
+except:
     last_page = 1
-
 
 # ?page=1
 
 url = lambda x: (src_url + '?page=' + x)
-episode_urls = list()
-titles = list()
-final = list()
+episode_urls: List[str] = list()
+titles: List[str] = list()
+final: List[str] = list()
 
 for i in range(1, last_page + 1):
     source = requests.get(url(str(i))).text
     souper = bs(source, 'lxml')
     episode_urls += souper.find_all('a', class_='episode-meta')
     titles += souper.find_all('div', class_="episode-thumbnail__container")
-print(len(episode_urls), len(titles))
+print('Found {} episodes'.format(len(titles)))
 k = 1
 # https://yugenani.me/watch/fef088de-e09b-4e07-91c2-c07066ee0c60/
 
-start = int(input('from episode number: ')) - 1
-end = int(input('till episode number: '))
+while True:
+    try:
+        start = int(input('from episode number: ')) - 1
+        end = int(input('till episode number: '))
+        break
+    except:
+        print('Enter correct input and in the specified range')
 
 try:
     for i, j in zip(titles, episode_urls[start:end]):
         final += [[str(k) + ' ' + i.img['title'], 'https://yugenani.me' + j['href']]]
         k += 1
 except Exception:
-    print('error')
+    print('ERROR: Title or link not found')
 
 for j, i in enumerate(episode_urls[start:end]):
     souping = bs(requests.get('https://yugenani.me' + i['href']).text, 'lxml')
@@ -54,17 +68,25 @@ for j, i in enumerate(final):
     a = 'https://gogo-stream.com/loadserver.php?id' + i[2].split('id')[-1]
     try:
         soupinger = bs(requests.get(a).text, 'lxml')
-        final[j][2] = (str(soupinger.find('div', class_="videocontent")).split(';')[-3].split('\n')[-3][:-2].strip()[1:])
-    except Exception:
+        final[j][2] = (
+        str(soupinger.find('div', class_="videocontent")).split(';')[-3].split('\n')[-3][:-2].strip()[1:])
+    except:
         pass
 for j, i in enumerate(final):
     final[j].pop(1)
 
-g = ['(HDP - mp4)', '(360P - mp4)', '(720P - mp4)', '(1080P - mp4)']
-print(g)
+g: List[str] = ['(HDP - mp4)', '(360P - mp4)', '(720P - mp4)', '(1080P - mp4)']
+print()
+for j,i in enumerate(g):
+    print(j,i)
 
-z = int(input('the index number from the list (eg: 1, 2, 3 ..): '))
-
+while True:
+    try:
+        z = int(input('Enter the index number from the list (eg: 0, 1, 2, 3 ..): '))
+        break
+    except:
+        print('Please enter the correct integer')
+        
 geek = input('geek mode? y/n')
 
 for j, i in enumerate(final):
@@ -103,8 +125,9 @@ def downloader(c):
         wget.download(i, os.getcwd())
 
 
-print(dow_urls)
-
+if geek == 'y':
+    print(dow_urls)
 
 if input('download now? y/n: ') == 'y':
     downloader(dow_urls)
+

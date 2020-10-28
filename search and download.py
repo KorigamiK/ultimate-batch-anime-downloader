f4835f959b6bc80eeaa038fe24aa9d0f0c74526a
Arjix <53124886+ArjixGamer@users.noreply.github.com>
2020-10-28 23:37:11 +0200

    improved gogoanime

--- search and download.py
+++ search and download.py
@@ -1,56 +1,74 @@
-from bs4 import BeautifulSoup as bs
+from bs4 import BeautifulSoup
 import requests
 from Downloader import downloader
+from tabulate import tabulate
 
 # ____________GogoAnime____________
-def getname(sample):
-    badname=sample.split('/category/')[-1]
-    goodname=' '.join(badname.split('-'))
-    return goodname
+def getname(url):
+    soup = BeautifulSoup(requests.get(url).text, "html.parser")
+    title = soup.find("meta", property="og:title")["content"].split(" at Go")[0]
+    return title
 
-#getname('https://gogoanime.so/category/ore-wo-suki-nano-wa-omae-dake-ka-yo-oretachi-no-game-set')
+
+# getname('https://gogoanime.so/category/ore-wo-suki-nano-wa-omae-dake-ka-yo-oretachi-no-game-set')
 def search_gogo():
-    user_input = input('enter anime: ')
-    query = "https://ajax.gogocdn.net/site/loadAjaxSearch?keyword={}&id=-1&link_web=https%3A%2F%2Fgogoanime.so%2F"
-    get = '+'.join(user_input.split(' '))
-    a = query.format(get).split(' ')
-    soup = bs(requests.get(a[0]).content,'lxml')
-    search_elements=list()
-    for j,i in enumerate(soup.find('div',class_='\\"thumbnail-recent_search\\"').find_all('a')):
-        badurl='/'.join(str(i['href']).split('\\/'))
-        goodurl=badurl.split('"')[1][:-1]
-        search_elements.append([getname(goodurl), goodurl])
-        print(j, getname(goodurl))
-    opt = int(input('Enter the anime number: '))
-    return search_elements[opt][1]
-# ________________Yugenani_______________
-def correct_format(x):
-    return x.replace(' ', '+')
+    user_input = input("Enter anime: ")
+
+    url = "https://www2.gogoanime.video//search.html"
+    html = requests.get(url, params={"keyword": user_input}).text
+    soup = BeautifulSoup(html, "html.parser")
+    search_results = soup.select("ul.items > li > p > a")
+    headers = ["SlNo", "Title"]
+    count = -1
+    table = []
+    links = []
+    for result in search_results:
+        count += 1
+        entry = [count, result.get("title")]
+        link = "https://www2.gogoanime.video" + result["href"]
+        table.append(entry)
+        links.append(link)
+    table = tabulate(table, headers, tablefmt="psql")
+    table = "\n".join(table.split("\n")[::-1])
+    print(table)
+    choice = input("Enter num: [0]: ")
+    choice = 0 if choice == "" else int(choice)
+    return links[choice]
+
+
+# ____________yugen____________
 def search_yugen(search):
     client = requests.session()
-    client.get('https://yugenani.me/anime/akudama-drive/watch/')
+    client.get("https://yugenani.me/anime/akudama-drive/watch/")
 
-    if 'csrftoken' in client.cookies:
+    if "csrftoken" in client.cookies:
         # Django 1.6 and up
-        csrftoken = client.cookies['csrftoken']
+        csrftoken = client.cookies["csrftoken"]
     else:
         # older versions
-        csrftoken = client.cookies['csrf']
-    #print(type(csrftoken))
-    response = client.post('https://yugenani.me/api/search/', data={"query":correct_format(search)},headers={'referer': 'https://yugenani.me/anime/akudama-drive/watch/',
-                                                                                                   'X-CSRFToken':csrftoken,                                                                                               
-                                                                                                   'X-Requested-With':'XMLHttpRequest'})
-    data= dict(response.json())
-    parsed_data = eval(data['query'].replace('null','None'))
-    for j,i in enumerate(parsed_data):
-        print(j,i['fields']['title'])
-    opt = int(input('enter anime number: '))
-    return 'https://yugenani.me/anime/'+parsed_data[opt]['fields']['slug']+'/watch/'
+        csrftoken = client.cookies["csrf"]
+    # print(type(csrftoken))
+    response = client.post(
+        "https://yugenani.me/api/search/",
+        data={"query": correct_format(search)},
+        headers={
+            "referer": "https://yugenani.me/anime/akudama-drive/watch/",
+            "X-CSRFToken": csrftoken,
+            "X-Requested-With": "XMLHttpRequest",
+        },
+    )
+    data = dict(response.json())
+    parsed_data = eval(data["query"].replace("null", "None"))
+    for j, i in enumerate(parsed_data):
+        print(j, i["fields"]["title"])
+    opt = int(input("enter anime number: "))
+    return "https://yugenani.me/anime/" + parsed_data[opt]["fields"]["slug"] + "/watch/"
 
-print('1 download from yugenani?')
-print('2 download from goganime?')
 
-if int(input('Enter option number: '))== 1:
-    downloader(search_yugen(input('Enter anime name: ')))
+print("1 download from yugenani?")
+print("2 download from goganime?")
+
+if int(input("Enter option number: ")) == 1:
+    downloader(search_yugen(input("Enter anime name: ")))
 else:
     downloader(search_gogo())
